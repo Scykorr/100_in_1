f3fb351e28c628fdbbb271abdeb9f61a814a7b51
Tony <fedosov-toxa@mail.ru>
2024-06-28 01:49:32 +0300

    some task

--- stepik/main.py
+++ stepik/main.py
@@ -1,9 +1,9 @@
-first_side = int(input())
-second_side = int(input())
-third_side = int(input())
-if first_side == second_side == third_side:
-    print('Равносторонний')
-elif (first_side == second_side) or (first_side == third_side) or (second_side == third_side):
-    print('Равнобедренный')
+first_inp_x_cel = int(input())
+first_inp_y_cel = int(input())
+second_inp_x_cel = int(input())
+second_inp_y_cel = int(input())
+if abs(first_inp_x_cel - second_inp_x_cel) == abs(
+        first_inp_y_cel - second_inp_y_cel) or first_inp_x_cel == second_inp_x_cel or first_inp_y_cel == second_inp_y_cel:
+    print('YES')
 else:
-    print('Разносторонний')
+    print('NO')
