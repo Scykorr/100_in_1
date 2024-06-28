fb3540a4b9bcd7b2cbe6dabc9b4ac17b74a0f79a
Tony <fedosov-toxa@mail.ru>
2024-06-29 01:44:41 +0300

    some task

--- stepik/main.py
+++ stepik/main.py
@@ -1,9 +1,4 @@
-first_inp_x_cel = int(input())
-first_inp_y_cel = int(input())
-second_inp_x_cel = int(input())
-second_inp_y_cel = int(input())
-if abs(first_inp_x_cel - second_inp_x_cel) == abs(
-        first_inp_y_cel - second_inp_y_cel) or first_inp_x_cel == second_inp_x_cel or first_inp_y_cel == second_inp_y_cel:
-    print('YES')
-else:
-    print('NO')
+inp_a = float(input())
+inp_b = float(input())
+result_s = inp_a * inp_b / 2
+print(result_s)
