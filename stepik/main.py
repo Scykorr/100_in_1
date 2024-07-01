5f2e6bf3a19d8b6d388a21deae3451fe6a8aadd3
Anton Fedosov <fedosov-toxa@mail.ru>
2024-07-01 17:32:40 +0300

    7.3 task v1

--- stepik/main.py
+++ stepik/main.py
@@ -1,4 +1,7 @@
-inp_a = float(input())
-inp_b = float(input())
-result_s = inp_a * inp_b / 2
-print(result_s)
+from math import log
+
+inp_num = int(input())
+total = 0
+for num in range(1, inp_num + 1):
+    total += (1 / num)
+print(total - log(inp_num))
