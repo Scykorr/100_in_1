017eb07b559529b496cfd15e5c5c4e26d62330e7
Tony <fedosov-toxa@mail.ru>
2024-07-02 00:15:08 +0300

    7.3 done

--- stepik/main.py
+++ stepik/main.py
@@ -1,7 +1,6 @@
-from math import log
-
-inp_num = int(input())
-total = 0
-for num in range(1, inp_num + 1):
-    total += (1 / num)
-print(total - log(inp_num))
+inp_amount_num = int(input())
+previous_num = 0
+current_num = 1
+for _ in range(1, inp_amount_num + 1):
+    print(current_num, end=' ')
+    previous_num, current_num = current_num, previous_num + current_num
