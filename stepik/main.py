5a9dc47ef95d60655b21694d986b1ac545d9ef01
Anton Fedosov <fedosov-toxa@mail.ru>
2024-07-02 17:28:08 +0300

    7.4 v1

--- stepik/main.py
+++ stepik/main.py
@@ -1,6 +1,1 @@
-inp_amount_num = int(input())
-previous_num = 0
-current_num = 1
-for _ in range(1, inp_amount_num + 1):
-    print(current_num, end=' ')
-    previous_num, current_num = current_num, previous_num + current_num
+inp_price = int(input())
