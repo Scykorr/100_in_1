ff4151f1d2b8ccc200bff317e55f6e3c5bf4d8e5
Anton Fedosov <fedosov-toxa@mail.ru>
2024-07-05 17:21:29 +0300

    9 stepik v1

--- stepik/main.py
+++ stepik/main.py
@@ -1,9 +1,2 @@
-for num in range(1729, 1730):
-    counter = 0
-    for num_a in range(1, 13):
-        for num_b in range(1, 13):
-            for num_c in range(1, 13):
-                for num_d in range(1, 13):
-                    if (num_a ** 3 + num_b ** 3) == (
-                            num_c ** 3 + num_d ** 3) == num and num_a != num_b and num_a != num_c and num_a != num_d and num_b != num_c and num_b != num_d and num_c != num_d:
-                        print(num)
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[::7])
